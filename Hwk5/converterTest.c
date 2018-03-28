8a7600e69417a56d7c0a1121315f5b76d69f4a82
brosand <brosand@gmail.com>
2018-03-28 17:01:11 -0400

    adding the inverter

---
+++ Hwk5/converterTest.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *keyConvert(unsigned int x, unsigned int y, unsigned int z) {
+    /* char *xx; */
+    /* char *yy; */
+    /* char *zz; */
+    /* xx = malloc(10); */
+    /* yy = malloc(10); */
+    /* zz = malloc(10); */
+    char * out;
+    out = malloc(31);
+    sprintf(out,"%010u", x);
+    sprintf(&out[10],"%010u", y);
+    sprintf(&out[20],"%010u", z);
+    return out;
+    /* atol(output, a, 10); */
+    /* return a; */
+    /* return atol() */
+} 
+unsigned int
+    hash(const char *str)
+    {
+        unsigned int hash = 5381;
+        int c;
+
+        while (c = *str++)
+            hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+        return hash % 300;
+    }
+
+
+int main() {
+    unsigned int t = -1;
+    const char *key = keyConvert(101,100,100);
+    unsigned int hashed = hash(key);
+    
+    printf("%u", hashed);
+}
