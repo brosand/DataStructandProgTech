adaa2e4c4178366e0b0412ed85c0d86558322180
Benjamin Rosand <br384@zoo.cs.yale.edu>
2018-04-09 15:36:44 -0400

    bunch of tries

--- Hwk7/oldArray.c
+++ Hwk7/oldArray.c
@@ -1,7 +1,7 @@
 
 #include <stdlib.h>
-#include <assert.h>
 #include "array.h"
+
 #define DIV (1)
 
 // Create a new array holding n values, all initially 0.
@@ -11,27 +11,19 @@
 
 struct array {
     size_t size;
-    int index;
-    int aggregate;
-    struct node *left;
-    struct node *right;
-    
+    int *items;
+    int (*combine)(int,int);
+    int *aggregates;
 };
 /* typedef struct array Array; */
 
 Array *arrayCreate(int (*combine)(int, int), size_t n){
     Array *a = malloc(sizeof(Array));
-    a->size = n;
-    a->index = 0;
-    for (int i = 0; i < n; i++) {
-        Array *aN = malloc(sizeof(Array))
-        araySet(a, i, 0)
-    }
-    /* a->items = calloc(n, sizeof(int)); */
-    /* a->itemsDIV = calloc(n/DIV, sizeof(int)); */
+    a->items = calloc(n, sizeof(int));
+    a->aggregates = calloc(n, sizeof(int));
     /* a->items = malloc(sizeof(int) * n); */
-    /* a->size = n; */
-    /* a->combine = combine; */
+    a->size = n;
+    a->combine = combine;
     return a;
 }
 
@@ -59,35 +51,14 @@ int arrayGet(const Array *a, size_t i) {
 // No effect if i is out of range.
 // Cost: O(log n).
 void arraySet(Array *a, size_t i, int v){
-    
-    Array *newNode;
-    
-    newNode = malloc(sizeof(newNode));
-    assert(newNode);
-    
-    newNode->index = i;
-    newNode->left = 0;
-    newNode->right = 0;
-    
-    for(;;) {
-        if(a->index > index) {
-            if (a->left) {
-            a = a->left;
-            } else {
-                a->left = newNode;
-                return;
-            }
-        } else {
-            if(a->right) {
-                a = a->right;
-            } else {
-                a->right = newNode;
-                return;
-            }
-        }
-        
+    if (a->size < i) {return;}
+    a->items[i] = v;
+    if(i == 0) {
+        a->aggregates[i] = 0;
+    } else {
+        a->aggregates[i] = a->combine(a->aggregates[i-1],v);
     }
-    
+    /* a->size++; */
 }
 
 // Return the result of aggregating the first k elements
@@ -97,27 +68,15 @@ void arraySet(Array *a, size_t i, int v){
 int arrayCombine(const Array *a, size_t k) {
     /* int agg = arrayGet(a,0); */
     if (a->size < k || k == 0) {k = a->size;};
-    int n = k/DIV;
-    int p = k%DIV;
     
-    int t = a->itemsDIV[0];
-    for (int i = 1; i < n * p; i++) {
-        t = a->combine(t, a->itemsDIV[i]);
-    }
-    if (n==0) {
-        t = arrayGet(a,k);
-        p++;
-    }
-    /* else {p++;} */
-    
-    int agg = a->combine(t, arrayGet(a, n*DIV + p));
+    /* int agg = arrayGet(a,0); */
     /* for (int i = 1; i < k; i++) { */
         /* agg = (a->combine(agg, arrayGet(a,i))); */
     /* } */
-    for (int i = n*DIV+p+1; i < k; i++) {
-        agg = a->combine(agg, arrayGet(a, i));
-    }
-    
-    return agg;
+    /* for (int i = 1; i < k; i++) { */
+        /* agg = a->combine(agg, arrayGet(a, i)); */
+    /* } */
+    return a->aggregates[k-1];
+    /* return agg; */
     
 }
